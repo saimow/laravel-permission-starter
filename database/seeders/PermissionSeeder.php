19af3ffdae957e90b2f15e661acb2c37f38d2ec7
Mohamed Hafidi <saimodev@gmail.com>
2023-04-16 17:55:41 +0000

    feat: add permissions seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'admin-dashboard-access',
+            'user-management-access',
+            'permission-access',
+            'role-access',
+            'role-create',
+            'role-edit',
+            'role-delete',
+            'user-access',
+            'user-edit',
+            'user-delete',
+            'post-access',
+            'post-create',
+            'post-edit',
+            'post-delete',
+        ];
+
+        foreach($permissions as $permission){
+            Permission::create(['name' => $permission]);
+        }
+        
+        $adminRole = Role::create(['name' => 'admin']);
+        $userRole = Role::create(['name' => 'user']);
+
+        $adminRole->givePermissionTo($permissions);
+
+        User::where('id', 1)->first()->assignRole($adminRole);
+        User::where('id', 2)->first()->assignRole($userRole);
+    }
+}
